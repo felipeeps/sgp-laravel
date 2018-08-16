fec58003899b69ce9f048893b077565b0af8f4ff
Felipe Pereira <felipeepereiras@gmail.com>
2018-08-16 19:44:48 -0300

    Trabalhando com as rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,26 @@
 <?php
+//Arquivo responsavel pelas rotas
+//Alguns Exemplos:
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+//Cria a rota passando o nome da chamada + função anonima
+Route::get('/empresa', function(){
+    return view('empresa');
+});
+
+Route::get('/contato', function(){
+    return 'Contato';
+})->name('rota.nomeada');
+
+//Usado para submissão de formulários
+Route::post('/post', function(){
+    return 'Route POST';
+});
+
+//Passando vários tipos de rota vou usar nesse endereço
+Route::match(['get', 'post'], '/match', function(){
+    return 'Route Match';
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Meu Projeto Laravel';
 });
