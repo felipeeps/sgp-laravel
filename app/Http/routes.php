8bfb0ef9fa1fa314aa92545b279be282ef571eba
Felipe Pereira <felipeepereiras@gmail.com>
2018-08-16 19:58:23 -0300

    Adicionando uma rota com parametro

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,26 +1,11 @@
 <?php
 //Arquivo responsavel pelas rotas
-//Alguns Exemplos:
-
-//Cria a rota passando o nome da chamada + função anonima
-Route::get('/empresa', function(){
-    return view('empresa');
-});
-
-Route::get('/contato', function(){
-    return 'Contato';
-})->name('rota.nomeada');
-
-//Usado para submissão de formulários
-Route::post('/post', function(){
-    return 'Route POST';
-});
-
-//Passando vários tipos de rota vou usar nesse endereço
-Route::match(['get', 'post'], '/match', function(){
-    return 'Route Match';
-});
 
 Route::get('/', function () {
-    return 'Meu Projeto Laravel';
+    return 'Projeto Laravel - TADS 6º Periodo';
+});
+
+//Criando uma rota de uma página de login
+Route::get('/home/{nome}', function ($nome) {
+    return "$nome, Seja bem vindo";
 });
