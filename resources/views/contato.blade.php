75f9aad0eecd7ae19fc1d993d8237e499c8f314b
Felipe Pereira <felipeepereiras@gmail.com>
2018-08-23 20:02:25 -0300

    Estilizando o form e adicionando links das paginas

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -58,9 +58,22 @@
         <h1>Entre em contato!</h1>
 
         <form action="" method="post">
-            <p>
+            <p class="form-group">
                 <label for="nome">Nome</label>
-                <input type="text" name="nome" placeholder="Entre com o seu nome">
+                <input class="form-control" type="text" name="nome" placeholder="Entre com o seu nome">
+            </p>
+            <p class="form-group">
+                <label for="email">E-mail</label>
+                <input class="form-control" type="email" name="email" placeholder="Entre com o seu E-mail">
+            </p>
+            <p class="form-group">
+                <label for="mensagem">Mensagem</label>
+                <textarea class="form-control" name="mensagem" id="mensagem" cols="30" rows="10"></textarea>
+            </p>
+            <p class="form-group">
+                <input class="btn btn-default" type="reset" value="Limpar">
+                <input class="btn btn-default" type="submit" value="Enviar">
+            </p>
         </form>
     </div><!-- /.container -->
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,19 +47,14 @@
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">Sobre</a></li>
-            <li><a href="#contact">Contato</a></li>
+            <li><a href="<?= url('sobre') ?>">Sobre</a></li>
+            <li><a href="<?= url('contato') ?>">Contato</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </nav>
 
     <div class="container">
-
-      <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
 
     </div><!-- /.container -->
 
